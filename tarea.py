3f984650e206e7920647ac148bbf16d5ac4a52d2
Gonzalo Pozzoli <gonzalopozzoli99@gmail.com>
2022-06-09 09:43:38 -0300

    tarea empezada

---
+++ tarea.py
@@ -0,0 +1,17 @@
+def crearCursada():
+  # Crea una cursada de materias
+  pass
+
+def agregarMateria():
+  # Crear una materia de la cursada 
+  pass
+
+def sacarMateria():
+  # saca una materia de la cursada
+  pass
+
+# Atributos de cada materia:
+#   Nombre
+#   profesor
+#   codigo
+#   annio
